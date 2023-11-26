1eb9063aebfa21df8098597ef28318b841a8d384
oolga77 <oemi.ua@gmail.com>
2023-11-26 20:52:59 +0100

    feat: therd task

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -56,8 +56,8 @@ function isString(value) {
  *   concatenateStrings('aa', '') => 'aa'.
  *   concatenateStrings('', 'bb') => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings(value1, value2) {
+  return ''.concat(value1, value2);
 }
 
 /**
